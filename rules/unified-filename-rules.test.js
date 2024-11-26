47bb111eaccea48924785f11766dc60b72268aa6
Baruch Odem <baruchiro@gmail.com>
2024-11-26 08:45:13 +0200

    create empty filenames

--- rules/unified-filename-rules.test.js
+++ rules/unified-filename-rules.test.js
@@ -1,97 +1,97 @@
-const rule = require('./unified-filename-rules');
-const { run } = require('./tests');
+const rule = require("./unified-filename-rules");
+const { run, useFilename } = require("./tests");
 
-run('unified-filename-rules', rule, {
+run("unified-filename-rules", rule, {
   valid: [
     {
-      filename: 'types.ts',
-      code: 'type MyType = {};',
+      ...useFilename("types.ts"),
+      code: "type MyType = {};",
     },
     {
-      filename: 'some-other-file.ts',
-      code: 'type MyType = {};',
+      ...useFilename("some-other-file.ts"),
+      code: "type MyType = {};",
     },
     {
-      code: '',
-      filename: 'SomeComponent.tsx',
+      code: "",
+      ...useFilename("SomeComponent.tsx"),
     },
     {
-      filename: 'consts.ts',
-      code: 'const myConst = 1;',
+      ...useFilename("consts.ts"),
+      code: "const myConst = 1;",
     },
     {
-      filename: 'constants.ts',
-      code: 'const myConst = 1;',
+      ...useFilename("constants.ts"),
+      code: "const myConst = 1;",
     },
     {
-      filename: 'const.ts',
-      code: 'const myConst = 1;',
+      ...useFilename("const.ts"),
+      code: "const myConst = 1;",
     },
     {
-      filename: 'UserInfoDialog.styles.ts',
-      code: 'const myConst = 1;',
+      ...useFilename("UserInfoDialog.styles.ts"),
+      code: "const myConst = 1;",
     },
   ],
   invalid: [
     {
-      filename: 'PermissionWithColor.types.ts',
-      code: 'type MyType = {};',
+      ...useFilename("PermissionWithColor.types.ts"),
+      code: "type MyType = {};",
       errors: [
         {
           message:
-            'To save time, you don’t need to name the file like this, just types.ts',
+            "To save time, you don’t need to name the file like this, just types.ts",
         },
       ],
     },
     {
-      filename: 'AnotherExample.types.ts',
-      code: 'type AnotherType = {};',
+      ...useFilename("AnotherExample.types.ts"),
+      code: "type AnotherType = {};",
       errors: [
         {
           message:
-            'To save time, you don’t need to name the file like this, just types.ts',
+            "To save time, you don’t need to name the file like this, just types.ts",
         },
       ],
     },
     {
-      code: '',
-      filename: 'SomeAction.tsx',
-      errors: [{ messageId: 'noActionFilename' }],
+      code: "",
+      ...useFilename("SomeAction.tsx"),
+      errors: [{ messageId: "noActionFilename" }],
     },
     {
-      filename: 'ComponentName.consts.ts',
-      code: 'const myConst = 1;',
-      errors: [{ messageId: 'constsFilename' }],
+      ...useFilename("ComponentName.consts.ts"),
+      code: "const myConst = 1;",
+      errors: [{ messageId: "constsFilename" }],
     },
     {
-      filename: 'ComponentName.constants.ts',
-      code: 'const myConst = 1;',
-      errors: [{ messageId: 'constsFilename' }],
+      ...useFilename("ComponentName.constants.ts"),
+      code: "const myConst = 1;",
+      errors: [{ messageId: "constsFilename" }],
     },
     {
-      filename: 'communities-permissions.const.ts',
-      code: 'const myConst = 1;',
-      errors: [{ messageId: 'constsFilename' }],
+      ...useFilename("communities-permissions.const.ts"),
+      code: "const myConst = 1;",
+      errors: [{ messageId: "constsFilename" }],
     },
     {
-      filename: 'style.ts',
-      code: 'const myConst = 1;',
-      errors: [{ messageId: 'stylesFilename' }],
+      ...useFilename("style.ts"),
+      code: "const myConst = 1;",
+      errors: [{ messageId: "stylesFilename" }],
     },
     {
-      filename: 'styles.ts',
-      code: 'const myConst = 1;',
-      errors: [{ messageId: 'stylesFilename' }],
+      ...useFilename("styles.ts"),
+      code: "const myConst = 1;",
+      errors: [{ messageId: "stylesFilename" }],
     },
     {
-      filename: 'styles.tsx',
-      code: 'const myConst = 1;',
-      errors: [{ messageId: 'stylesFilename' }],
+      ...useFilename("styles.tsx"),
+      code: "const myConst = 1;",
+      errors: [{ messageId: "stylesFilename" }],
     },
     {
-      filename: 'component.styles.ts',
-      code: 'const myConst = 1;',
-      errors: [{ messageId: 'stylesFilename' }],
+      ...useFilename("component.styles.ts"),
+      code: "const myConst = 1;",
+      errors: [{ messageId: "stylesFilename" }],
     },
   ],
 });
